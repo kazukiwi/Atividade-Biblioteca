1f73ea67599f1b9e6df3e40d4ec7ca1fd0446fcb
Kazuki Vitor Shiroma <kazukishiroma06@gmail.com>
2025-09-29 10:03:53 -0300

    Etapa 5 - Função de remoção de livros implementada e atualização try except

--- funcoes.py
+++ funcoes.py
@@ -1,4 +1,5 @@
 import sqlite3 as sql
+import funcoes as fun
 conexao = sql.connect("biblioteca.db")
 cursor = conexao.cursor()
 
@@ -14,6 +15,10 @@ CREATE TABLE IF NOT EXISTS livros(
 """)
 print("Tabela criada com sucesso!")
 
+def conexaoo():
+    conexao = sql.connect("biblioteca.db")
+    cursor = conexao.cursor()
+
 def menu():
     print("\nMenu Biblioteca!")
 
@@ -24,37 +29,63 @@ def menu():
     print("5. Sair do sistema")
 
 def listar_livros():
-    cursor.execute("""
-    SELECT * FROM biblioteca
-    """)
-    for linha in cursor.fetchall():
-        print(f"ID - {linha[0]} | Título - {linha[1]} | Autor - {linha[2]} | Ano - {linha[3]} | Disponibilidade - {linha[4]}")
-
+    try:
+        cursor.execute("""
+        SELECT * FROM biblioteca
+        """)
+        for linha in cursor.fetchall():
+            print(f"ID - {linha[0]} | Título - {linha[1]} | Autor - {linha[2]} | Ano - {linha[3]} | Disponibilidade - {linha[4]}")
+    except Exception as erro:
+        print(f"Erro ao tentar listar os livros: {erro}")
 
 def cadastrar_livros():
     titulo = input("Digite o Título do livro: ")
     autor = input("Digite o autor do livro: ")
     ano = int(input("Digite o ano de lançamento do livro: "))
     disponivel = "sim"
-    cursor.execute("""
-    INSERT INTO livros (titulo, autor, ano, disponível)
-    VALUES (?, ?, ?, ?)
-""", (titulo, autor, ano, disponivel))
-    conexao.commit()
+    try:
+        cursor.execute("""
+        INSERT INTO livros (titulo, autor, ano, disponível)
+        VALUES (?, ?, ?, ?)
+        """, (titulo, autor, ano, disponivel))
+        conexao.commit()
+    except Exception as erro:
+        print(f"Erro ao tentar cadastrar livro: {erro}")
     
 def atualização_disponibilidade():
     id_atualizar = input("Digite o id que queira modificar: ")
     nao_sim = int(input("Deseja disponibilizar (1) ou não disponibilizar (0): "))
-    if nao_sim == 0:
-        cursor.execute("""
-        SET disponível = ? FROM WHERE id = ?
-        """, ("Não", id_atualizar))
+    try:
+        if nao_sim == 0:
+            cursor.execute("""
+            UPDATE biblioteca
+            SET disponível = ? FROM WHERE id = ?
+            """, ("Não", id_atualizar))
+            conexao.commit()
+
+        elif nao_sim == 1:
+            cursor.execute("""
+            UPDATE biblioteca
+            SET disponível = ? FROM WHERE id = ?
+            """, ("Sim", id_atualizar))
+            conexao.commit()
+    except Exception as erro:
+        print(f"Erro ao tentar atualizar a disponibilidade: {erro}")
+
+def remover_livro():
+    id_livro = int(input("Digite o ID do aluno: "))
+    try:
+        fun.conexaoo()
+        cursor.execute("DELETE FROM biblioteca WHERE id = ?", (id_livro))
         conexao.commit()
 
-    elif nao_sim == 1:
-        cursor.execute("""
-        UPDATE biblioteca
-        SET disponível = ? FROM WHERE id = ?
-        """, ("Sim", id_atualizar))
-        conexao.commit()
-    
+        if cursor.rowcount > 0:
+            print("Livro excluido com sucesso!")
+        else:
+            print("Nenhum livro encontrado com o ID fornecido")
+    except Exception as error:
+        print(f"Error encontrado! ERRO {error}")
+
+    finally:
+        if conexao:
+            conexao.close()
